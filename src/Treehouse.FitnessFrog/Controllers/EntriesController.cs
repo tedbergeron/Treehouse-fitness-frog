1690ddacd98d35c535a7d82c0bbce3ed5188c096
Ted Bergeron <ted_bergeron@yahoo.com>
2016-10-05 13:53:28 -0700

    Updated the Add Entry controller and view to use the entry data model

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -41,16 +41,28 @@ namespace Treehouse.FitnessFrog.Controllers
 
         public ActionResult Add()
         {
-            return View();
+            var entry = new Entry();
+            {
+                entry.Date = DateTime.Today;
+            }
+
+
+            return View(entry);
         }
 
         [HttpPost]
-        public ActionResult Add(DateTime? date,
-            int? activityId, double? duration,
-            Entry.IntensityLevel? intensity, 
-            bool? exclude, string notes)
+        public ActionResult Add(Entry entry)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.AddEntry(entry);
+
+                // TODO Display the Entries list page.
+
+            }
+
+
+            return View(entry);
         }
 
 
